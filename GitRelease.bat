e0e603dc8a4d79a214550afbaf4ac0cc0760f72b
stackprobe <stackprobes@gmail.com>
2018-10-03 23:46:13 +0900

    coffee break

--- GitRelease.bat
+++ GitRelease.bat
@@ -1,5 +1,5 @@
 IF NOT EXIST .\GitRelease.bat GOTO END
 CALL qq
-C:\Factory\SubTools\GitFactory.exe /ow . C:\home\GitHub\NetworkTest
+C:\Factory\SubTools\GitFactory.exe /ow . C:\huge\GitHub\NetworkTest
 rem CALL C:\var\go.bat
 :END
